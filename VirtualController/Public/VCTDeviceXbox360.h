5d23d1fc107393ed46bf50ba45ae8fc088db53ed
Ian Gregory <justcheesy@outlook.com>
2024-09-17 21:01:09 -0400

    Better mapping description for 360 devices

--- VirtualController/Public/VCTDeviceXbox360.h
+++ VirtualController/Public/VCTDeviceXbox360.h
@@ -4,7 +4,25 @@ typedef struct {
     NSPoint leftStick;
     NSPoint rightStick;
     struct { CGFloat left, right; } triggers;
-    // A, B, X, Y, LB, RB, LS, RS, Menu, Share, nothing?, Up, Down, Left, Right, nothing?
+    
+    // 0: A
+    // 1: B
+    // 2: X
+    // 3: Y
+    // 4: LB
+    // 5: RB
+    // 6: LS
+    // 7: RS
+    // 8: Start
+    // 9: Back
+    // A: nothing?
+    // B: Up
+    // C: Down
+    // D: Left
+    // E: Right
+    // F: nothing?
+    //
+    // https://gamepadviewer.net/custom-skin/xbox-360
     struct { BOOL pressed : 1; } buttons[16];
 } VCTDeviceStateXbox360;
 
